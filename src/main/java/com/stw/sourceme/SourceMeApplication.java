c90a065ca6f51b0d274d6f2ce6cd27d664108d71
STW5 <xodnd8384@gmail.com>
2025-12-23 20:53:02 +0900

    init commit

---
+++ src/main/java/com/stw/sourceme/SourceMeApplication.java
@@ -0,0 +1,13 @@
+package com.stw.sourceme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourceMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stw/sourceme/SourceMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stw.sourceme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourceMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
