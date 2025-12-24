edabd618c442d01e870a3fdd43904c80ce108e65
STW5 <xodnd8384@gmail.com>
2025-12-24 16:23:53 +0900

    chore: validation 의존성 및 auditing/포트 설정

--- src/main/java/com/stw/sourceme/SourceMeApplication.java
+++ src/main/java/com/stw/sourceme/SourceMeApplication.java
@@ -2,14 +2,23 @@ package com.stw.sourceme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
 public class SourceMeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SourceMeApplication.class, args);
 	}
 
+	@Bean
+	public DateTimeProvider auditingDateTimeProvider() {
+		return () -> Optional.of(LocalDateTime.now());
+	}
 }
